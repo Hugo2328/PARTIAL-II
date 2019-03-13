dd1ccd614746ae69d5e56eee781c1e77e8bf1351
Hugo2328 <46615084+Hugo2328@users.noreply.github.com>
2019-03-13 15:11:05 -0500

    Actualizado
    Actualizado

--- includes/_db.php
+++ includes/_db.php
@@ -5,25 +5,10 @@ $db = "equipop4_desarrolloweb";
 $user = "equipop4";
 $password = "Proyectounid.2019"; 
 $mysqli = new mysqli($server, $user, $password, $db);
-if ($mysqli->connect_errno) {
-    echo "Lo sentimos, este sitio web está experimentando problemas.";
-    echo "Error: Fallo al conectarse a MySQL debido a: \n";
-    echo "Errno: " . $mysqli->connect_errno . "\n";
-    
-    exit;
+if($mysqli === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-/*
-try{
 
-    
-
-	$db = new PDO('mysql:host=209.59.139.37; dbname=equipop4_desarrolloweb', 'equipop4', 'Proyectounid.2019');
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	} catch(PDOException $ex) {
-    echo "An Error occured!"; 
-    some_logging_function($ex->getMessage());
-}
-*/
 
 
 
